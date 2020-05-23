a5711bd9f5a86f92aa3c114daf5731bd1507584c
Kristof <kristof.kovacs@telenav.com>
2020-05-23 12:40:31 +0300

    new version

--- demo.py
+++ demo.py
@@ -3,23 +3,6 @@ import time
 
 from ktimeout import timeout
 
-# TEST WITH CLASS
-class T:
-    def func(self):
-        return timeout.run(
-            timeout.partial(self.__func, 1, kw=3),
-            2
-        )
-
-    def __func(self, i: int, kw: int = 0):
-        print(kw)
-
-        return i
-
-try:
-    print('res', T().func())
-except Exception as e:
-    print(e)
 
 
 # TESTS WITH GLOBAL FUNCS
@@ -30,27 +13,29 @@ def func_with_arguments(sleep_time: float, extra_print: Optional[str] = None):
         print('Sleeping', sleep_time, 'sec', extra_print or '')
 
 def func():
-    func_with_arguments(0.5, extra_print='called from func()')
+    func_with_arguments(0.25, extra_print='called from func()')
 
 def func_with_return():
-    time.sleep(1)
+    time.sleep(0.25)
 
     return 'return_val'
 
 try:
-    timeout.run(func, 2)
+    timeout.run(func, 1)
 except Exception as e:
     print(e)
 
 try:
     timeout.run(
         timeout.partial(func_with_arguments, 0.25, extra_print='extra'),
-        2
+        1
     )
 except Exception as e:
     print(e)
 
 try:
-    print(timeout.run(func_with_return, 2))
+    print(timeout.run(func_with_return, 1))
 except Exception as e:
     print(e)
+
+time.sleep(5)
